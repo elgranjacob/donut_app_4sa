397eb6bd701cf1cabecc95540a1111ad08aea6b1
Jacob <jacobismael6@gmail.com>
2025-02-16 15:18:46 -0600

    Add text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatefulWidget { //los datos que va cambiando el usuario y por lo mismo se van actualizando (cuando necesitamos cambios)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
   const HomePage({super.key});
 
   @override
@@ -13,12 +13,46 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(//icono de la izquierda
-          Icons.menu,
-          color: Colors.grey[800],
-        ),
-        actions: [Icon(Icons.person)],
+        //icono de la izquierda
+        leading: Icon(
+          Icons.menu, 
+          color: Colors.grey[800]
+          ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
+          ],
       ),
+      body: Column(
+        children: [
+        //1. Texto principal (Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(
+                fontSize: 32
+              ),),
+              Text('Eat', style: TextStyle(
+                //Tamaño de letra
+                fontSize: 32,
+                // Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+              ),),
+            ],
+          ),
+        )
+        //2. Pestañas (TabBar)
+
+        //3. Contenido de pestañas (TapBarView)
+
+        //4. Carrito (Cart)
+        ],
+      )
     );
   }
 }
