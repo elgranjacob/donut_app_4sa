ddcfd1e4f2b327e2401ebf7d1513e8304cbefdcb
Jacob <jacobismael6@gmail.com>
2025-04-03 13:11:03 -0600

    Shopping Cart Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,9 +4,11 @@ import 'package:donut_app_4sa/tabs/pancakes_tab.dart';
 import 'package:donut_app_4sa/tabs/pizza_tab.dart';
 import 'package:donut_app_4sa/tabs/smothies_tab.dart';
 import 'package:donut_app_4sa/utils/my_tab.dart';
+import 'package:donut_app_4sa/utils/shopping_cart.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget { //los datos que va cambiando el usuario y por lo mismo se van actualizando (cuando necesitamos cambios)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+class HomePage extends StatefulWidget {
+  //los datos que va cambiando el usuario y por lo mismo se van actualizando (cuando necesitamos cambios)
   const HomePage({super.key});
 
   @override
@@ -15,68 +17,70 @@ class HomePage extends StatefulWidget { //los datos que va cambiando el usuario 
 
 class _HomePageState extends State<HomePage> {
   List<Widget> myTabs = [
-   const MyTab(iconPath: 'lib/icons/donut.png'),
-   const MyTab(iconPath: 'lib/icons/burger.png'),
-   const MyTab(iconPath: 'lib/icons/pancakes.png'),
-   const MyTab(iconPath: 'lib/icons/pizza.png'),
-   const MyTab(iconPath: 'lib/icons/smoothie.png')
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
+    const MyTab(iconPath: 'lib/icons/smoothie.png')
   ];
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          //icono de la izquierda
-          leading: Icon(
-            Icons.menu, 
-            color: Colors.grey[800]
-            ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 24.0),
-              child: Icon(Icons.person),
-            )
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            //icono de la izquierda
+            leading: Icon(Icons.menu, color: Colors.grey[800]),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: Icon(Icons.person),
+              )
             ],
-        ),
-        body: Column(
-          children: [
-          //1. Texto principal (Text)
-         const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              children: [
-                Text('I want to ', style: TextStyle(
-                  fontSize: 32
-                ),),
-                Text('Eat', style: TextStyle(
-                  //Tamaño de letra
-                  fontSize: 32,
-                  // Negritas
-                  fontWeight: FontWeight.bold,
-                  //Subrayado
-                  decoration: TextDecoration.underline,
-                ),),
-              ],
-            ),
           ),
-          //2. Pestañas (TabBar)
-          TabBar(tabs: myTabs),
-          //3. Contenido de pestañas (TapBarView)
-          Expanded(
-            child: TabBarView(children: [
-                DonutTab(),
-                BurgerTab(),
-                SmothiesTab(),
-                PanCakesTab(),
-                PizzaTab()
-            ]),
-          )
-          //4. Carrito (Cart)
-          ],
-        )
-      ),
+          body: Column(
+            children: [
+              //1. Texto principal (Text)
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  children: [
+                    Text(
+                      'I want to ',
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    Text(
+                      'Eat',
+                      style: TextStyle(
+                        //Tamaño de letra
+                        fontSize: 32,
+                        // Negritas
+                        fontWeight: FontWeight.bold,
+                        //Subrayado
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //2. Pestañas (TabBar)
+              TabBar(tabs: myTabs),
+              //3. Contenido de pestañas (TapBarView)
+              Expanded(
+                child: TabBarView(children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmothiesTab(),
+                  PanCakesTab(),
+                  PizzaTab()
+                ]),
+              ),
+              //4. Carrito (Cart)
+              const ShoppingCart()
+            ],
+          )),
     );
   }
 }
+
--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -4,7 +4,7 @@ class DonutTile extends StatelessWidget {
   final String donutFlavor;
   final String donutStore;
   final String donutPrice;
-  final dynamic donutColor; //dynamic porque es para color
+  final dynamic donutColor;
   final String imageName;
 
   const DonutTile({
@@ -15,70 +15,77 @@ class DonutTile extends StatelessWidget {
     required this.donutColor,
     required this.imageName,
   });
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Container(
-          //color: donutColor[50],
-          decoration: BoxDecoration(
-            color: donutColor[50],
-            borderRadius: BorderRadius.circular(24),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: donutColor[100],
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(24),
-                          topRight: Radius.circular(24)),
+        decoration: BoxDecoration(
+          color: donutColor[50],
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: donutColor[100],
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
                     ),
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
-                    child: Text('\$45',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: donutColor[800])),
                   ),
-                ],
-              ),
-              //*Donut price
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 24, vertical: 0),
-                child: Image.asset(imageName),
-
-              //*Donut flavor
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  donutFlavor,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
-                      color: donutColor[800]),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                  child: Text('\$45',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: donutColor[800])),
                 ),
+              ],
+            ),
+            //*Donut Image
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 0),
+                child: Image.asset(imageName),
               ),
-              //Text
-              Text(
-                donutStore,
-                style: TextStyle(color: Colors.grey[600], fontSize: 20),
+            ),
+            //*Donut Flavor
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                donutFlavor,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    color: donutColor[800]),
               ),
-              //*Icons
-              Row(
+            ),
+            //* Store Name
+            Text(
+              donutStore,
+              style: TextStyle(color: Colors.grey[600], fontSize: 20),
+            ),
+            const Spacer(), // Empuja los íconos hacia abajo
+            //*Icons
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                Icon(Icons.favorite_border,color: Colors.red[400]),
-                Icon(Icons.add, color: Colors.grey)
-              ],)
-            ],
-          )),
+                  Icon(Icons.favorite_border, color: Colors.red[400]),
+                  Icon(Icons.add, color: Colors.grey),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
